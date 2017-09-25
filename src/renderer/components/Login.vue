9b481cc6ac6a093461bbe887fd4858f7889f25dd
dam <240242398@qq.com>
2017-09-25 16:43:22 +0800

    init

---
+++ src/renderer/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        abc
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+
+            }
+        }
+    }
+</script>
