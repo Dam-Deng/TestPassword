480cdab6c6d08bde2621c5b36bc56e5ab36a0aff
dam <240242398@qq.com>
2017-09-30 18:35:53 +0800

    update menu

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-import {app, BrowserWindow} from 'electron' // eslint-disable-line
+import {app, BrowserWindow, Menu} from 'electron' // eslint-disable-line
 
 /**
  * Set `__static` path to static files in production
@@ -12,6 +12,172 @@ let mainWindow;
 const winURL = process.env.NODE_ENV === 'development'
     ? 'http://localhost:9080'
     : `file://${__dirname}/index.html`;
+
+
+const template = [
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Undo',
+                accelerator: 'CmdOrCtrl+Z',
+                role: 'undo'
+            },
+            {
+                label: 'Redo',
+                accelerator: 'Shift+CmdOrCtrl+Z',
+                role: 'redo'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Cut',
+                accelerator: 'CmdOrCtrl+X',
+                role: 'cut'
+            },
+            {
+                label: 'Copy',
+                accelerator: 'CmdOrCtrl+C',
+                role: 'copy'
+            },
+            {
+                label: 'Paste',
+                accelerator: 'CmdOrCtrl+V',
+                role: 'paste'
+            },
+            {
+                label: 'Select All',
+                accelerator: 'CmdOrCtrl+A',
+                role: 'selectall'
+            },
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click: function(item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.reload();
+                }
+            },
+            {
+                label: 'Toggle Full Screen',
+                accelerator: (function() {
+                    if (process.platform === 'darwin')
+                        return 'Ctrl+Command+F';
+                    else
+                        return 'F11';
+                })(),
+                click: function(item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+                }
+            },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: (function() {
+                    if (process.platform === 'darwin')
+                        return 'Alt+Command+I';
+                    else
+                        return 'Ctrl+Shift+I';
+                })(),
+                click: function(item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.toggleDevTools();
+                }
+            },
+        ]
+    },
+    {
+        label: 'Window',
+        role: 'window',
+        submenu: [
+            {
+                label: 'Minimize',
+                accelerator: 'CmdOrCtrl+M',
+                role: 'minimize'
+            },
+            {
+                label: 'Close',
+                accelerator: 'CmdOrCtrl+W',
+                role: 'close'
+            },
+        ]
+    },
+    {
+        label: 'Help',
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click: function() { shell.openExternal('http://electron.atom.io') }
+            },
+        ]
+    },
+];
+
+if (process.platform === 'darwin') {
+    const name = app.getName();
+    template.unshift({
+        label: name,
+        submenu: [
+            {
+                label: 'About ' + name,
+                role: 'about'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Services',
+                role: 'services',
+                submenu: []
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Hide ' + name,
+                accelerator: 'Command+H',
+                role: 'hide'
+            },
+            {
+                label: 'Hide Others',
+                accelerator: 'Command+Shift+H',
+                role: 'hideothers'
+            },
+            {
+                label: 'Show All',
+                role: 'unhide'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Quit',
+                accelerator: 'Command+Q',
+                click: function() { app.quit(); }
+            },
+        ]
+    });
+    const windowMenu = template.find(function(m) { return m.role === 'window' })
+    if (windowMenu) {
+        windowMenu.submenu.push(
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Bring All to Front',
+                role: 'front'
+            }
+        );
+    }
+}
+
 
 function createWindow() {
     /**
@@ -30,6 +196,9 @@ function createWindow() {
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
+
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
 }
 
 app.on('ready', createWindow);
