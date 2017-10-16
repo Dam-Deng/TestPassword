bf8662d412b66f55eae46b213736e13ec8ad076a
dam <240242398@qq.com>
2017-10-17 00:14:15 +0800

    update  e2e test

--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -1,13 +1,55 @@
 import utils from '../utils';
 
 describe('Launch', function () {
-  beforeEach(utils.beforeEach);
-  afterEach(utils.afterEach);
+    beforeEach(utils.beforeEach);
+    afterEach(utils.afterEach);
 
-  it('shows the proper application title', function () {
-    return this.app.client.getTitle()
-      .then((title) => {
-        expect(title).to.equal('test-password');
-      });
-  });
+    it('窗口基本情况', async function () {
+        const win = this.app.browserWindow;
+        let client = this.app.client;
+
+        expect(await client.getWindowCount()).to.equal(1);
+        expect(await win.isMinimized()).to.equal(false);
+        expect(await win.isDevToolsOpened()).to.equal(false);
+        expect(await win.isVisible()).to.equal(true);
+
+        const {width, height} = await win.getBounds();
+        expect(width).to.above(0);
+        expect(height).to.above(0);
+
+        expect(await client.getTitle()).to.equal('test-password');
+    });
+
+    it('项目流程', async function () {
+        // 设置测试的超时时间，并且浏览窗口要顶置
+        this.timeout(150000);
+
+        let client = this.app.client;
+        // 登入流程
+        expect(await client.getUrl()).to.equal(utils.indexPath);
+        await client.setValue('#password', '123456');
+        await this.app.webContents.sendInputEvent({type: 'keyUp', keyCode: 'Return'});
+        await expect(await client.getUrl()).to.equal(utils.indexPath + 'home');
+
+        // 创建流程
+        await client.waitForExist('#addBtn');
+        await client.click('#addBtn');
+        await expect(await client.getUrl()).to.equal(utils.indexPath + 'security-form');
+
+        await client.waitForExist('#formTitle');
+        expect(await client.getText('#formTitle')).to.equal('新增');
+
+        await client.setValue('#name', '测试名称');
+        await client.setValue('#account', 'my_account');
+        await client.click('#genPassword');
+        await client.click('#confirmGenPassword');
+        await client.setValue('#url', 'http://www.baidu.com');
+        await client.setValue('#remark', '测试数据');
+        await client.click('#secrurityFormSubmitBtn');
+        await expect(await client.getUrl()).to.equal(utils.indexPath + 'home');
+
+        await client.waitForExist('#addBtn');
+        expect(await client.getText('#securityList')).to.be.contain('测试名称');
+
+    });
 });
--- test/e2e/utils.js
+++ test/e2e/utils.js
@@ -1,23 +1,25 @@
 import electron from 'electron'
-import { Application } from 'spectron'
+import path from 'path';
+import {Application} from 'spectron'
 
 export default {
-  afterEach () {
-    this.timeout(10000)
+    indexPath: `file://${path.resolve(__dirname, '../../')}/dist/electron/index.html#/`,
+    afterEach() {
+        this.timeout(10000)
 
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop()
+        if (this.app && this.app.isRunning()) {
+            return this.app.stop()
+        }
+    },
+    beforeEach() {
+        this.timeout(10000)
+        this.app = new Application({
+            path: electron,
+            args: ['dist/electron/main.js'],
+            startTimeout: 10000,
+            waitTimeout: 10000
+        })
+
+        return this.app.start()
     }
-  },
-  beforeEach () {
-    this.timeout(10000)
-    this.app = new Application({
-      path: electron,
-      args: ['dist/electron/main.js'],
-      startTimeout: 10000,
-      waitTimeout: 10000
-    })
-
-    return this.app.start()
-  }
 }
